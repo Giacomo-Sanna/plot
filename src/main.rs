2a482569bc4b97eca86317f907ccbb0daba07adb
Giacomo-Sanna <50588113+Giacomo-Sanna@users.noreply.github.com>
2022-12-18 19:52:59 +0100

    First version

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod lissajous_curve;
+mod graph_image1;
+mod graph_image2;
+mod plot;
+
+fn main() {
+    // let _ = lissajous_curve::run();
+    graph_image1::run();
+    // let _ = graph_image2::run();
+    //plot::run();
+}
